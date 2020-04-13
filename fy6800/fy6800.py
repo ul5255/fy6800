43cb630c8ca79352b02f374225dcc532620bc3cc
Uwe Lange <uwe_lange@web.de>
2020-04-14 00:00:50 +0200

    change it to a class

--- fy6800/fy6800.py
+++ fy6800/fy6800.py
@@ -1,63 +1,41 @@
-import math
-import serial
-import serial.tools.list_ports
+import math, serial, serial.tools.list_ports
 
 
-__port = None
-def req(cmd): __port.write((cmd+'\n').encode())
-def raw(bytes): __port.write(bytes)
-def resp(): return __port.readall().decode().rstrip('\n')
-def ch(i): return ['M', 'F'][i-1]
+ch = {1: 'M', 2: 'F'}
+def dBm(v): return math.sqrt(math.pow(10.0, v/10.0)/2.5)*2.0
+def MHz(v): return 1000*kHz(v)
+def kHz(v): return 1000*Hz(v)
+def Hz(v): return 1000*mHz(v)
+def mHz(v): return 1000*uHz(v)
+def uHz(v): return v
 
-def open():
-    global __port
-    for p in serial.tools.list_ports.grep('1A86'):
-        __port = serial.Serial(port=p.device, baudrate=115200, timeout=0.7)
-        req('UMO')
-        if resp().startswith('FY6800'): break
-        __port.close()
-        __port = None
-    assert(__port is not None)
+class FY6800(object):
+    def req(self, cmd): self.port.write((cmd+'\n').encode())
+    def raw(self, bytes): self.port.write(bytes)
+    def resp(self): return self.port.readall().decode().rstrip('\n')
 
-def dBm(v): math.sqrt(math.pow(10.0, v/10.0)/2.5)*2.0
-def a(n, v): req(f"W{ch(n)}A{v:02.4f}")
+    def __init__(self):
+        self.port = None
+        for p in serial.tools.list_ports.grep('1A86'):
+            self.port = serial.Serial(port=p.device, baudrate=115200, timeout=0.7)
+            self.req('UMO')
+            if self.resp().startswith('FY6800'): break
+            self.port.close()
+            self.port = None
+        assert(self.port is not None)
 
-def MHz(v): 1000*kHz(v)
-def kHz(v): 1000*Hz(v)
-def Hz(v): 1000*mHz(v)
-def mHz(v): 1000*uHz(v)
-def uHz(v): v
-def f(n, frq): return f"W{ch(n)}F{frq:014d}"
+    def a(self, n, v): self.req(f"W{ch[n]}A{v:02.4f}")
 
-def arb(i, one_period):
-    assert(i in range(1, 17))
-    assert(len(one_period) == 2**13)
-    # byte n: lower 8bit, byte n+1: upper 6bit
-    seq = []
-    for v in one_period: seq += [v & 255, (v >> 8) & 63]
-    seq = bytearray(seq)
-    req(f"DDS_WAVE{chr(i-1 + ord('1'))}")
-    assert(resp() == 'W')
-    chunk_size = 2**8
-    for j in range(0, len(seq), chunk_size): raw(seq[j:j+chunk_size])
-    assert(resp() == 'HN')
+    def f(self, n, frq): self.req(f"W{ch[n]}F{frq:014d}")
 
-#   _
-#  -/-\_---->
-up = [v << 3 for v in range(2**11)]
-down = [v << 3 for v in range(2**11-1, -1, -1)]
-
-ramp = []
-ramp += up
-ramp += [2**14 - 1] * 2**11
-ramp += down
-ramp += [0] * 2**11
-
-open()
-
-arb(1, ramp)
-req('WMW00')
-a(1, dBm(0))
-f(1, MHz(1)+kHz(456))
-
-__port.close()
+    def arb(self, i, one_period):
+        assert(i in range(1, 17))
+        assert(len(one_period) == 2**13)
+        # byte n: lower 8bit, byte n+1: upper 6bit
+        seq = bytearray([])
+        for v in one_period: seq += bytearray([v & 255, (v >> 8) & 63])
+        self.req(f"DDS_WAVE{chr(i-1 + ord('1'))}")
+        assert(self.resp() == 'W')
+        chunk_size = 2**8
+        for j in range(0, len(seq), chunk_size): self.raw(seq[j:j+chunk_size])
+        assert(resp() == 'HN')
---
+++ fy6800/hello.py
@@ -0,0 +1,17 @@
+from fy6800 import FY6800, dbM, MHz, kHz, Hz
+
+
+g = FY6800()
+
+#    _
+#  -/-\_---->
+up = [v << 3 for v in range(2**11)]
+high = [2**14 - 1] * 2**11
+down = [v << 3 for v in range(2**11-1, -1, -1)]
+low = [0] * 2**11
+
+g.arb(1, up+high+down+low)
+
+g.req('WMW00')
+g.a(1, dBm(0))
+g.f(1, MHz(1)+kHz(456))
