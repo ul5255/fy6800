2d14d24e631c347260a5e87f703b586c6250ba9f
Uwe Lange <uwe_lange@web.de>
2020-04-14 23:39:36 +0200

    read back from FY6800 after sending each command even its just \n

--- fy6800/fy6800.py
+++ fy6800/fy6800.py
@@ -10,31 +10,28 @@ def mHz(v): return 1000*uHz(v)
 def uHz(v): return v
 
 class FY6800(object):
-    def req(self, cmd): self.port.write((cmd+'\n').encode())
-    def resp(self): return self.port.readall().decode().rstrip('\n')
+    def w(self, cmd): self.port.write((cmd+'\n').encode())
+    def r(self): return self.port.readall().decode().rstrip('\n')
+    def cmd(self, cmd): self.w(cmd); return self.r()
 
     def __init__(self):
         self.port = None
         for p in serial.tools.list_ports.grep('1A86'):
             self.port = serial.Serial(port=p.device, baudrate=115200, timeout=0.7)
-            self.req('UMO')
-            if self.resp().startswith('FY6800'): break
+            if self.cmd('UMO').startswith('FY6800'): break
             self.port.close()
             self.port = None
         assert(self.port is not None)
 
-    def a(self, n, v): self.req(f"W{ch[n]}A{v:02.4f}")
-
-    def f(self, n, frq): self.req(f"W{ch[n]}F{frq:014d}")
+    def a(self, n, v): self.cmd(f"W{ch[n]}A{v:02.4f}")
+    def f(self, n, frq): self.cmd(f"W{ch[n]}F{frq:014d}")
+    def off(self, n): self.cmd(f"W{ch[n]}N0")
+    def on(self, n): self.cmd(f"W{ch[n]}N1")
 
     def arb(self, i, one_period):
         assert(i in range(1, 65))
         assert(len(one_period) == 2**13)
+        assert(self.cmd(f"DDS_WAVE{i:d}") == 'W')
         # byte n: lower 8bit, byte n+1: upper 6bit
-        seq = bytearray([])
-        for v in one_period: seq += bytearray([v & 255, (v >> 8) & 63])
-        self.req(f"DDS_WAVE{i:d}")
-        assert(self.resp() == 'W')
-        chunk_size = 2**8
-        for j in range(0, len(seq), chunk_size): self.port.write(seq[j:j+chunk_size])
-        assert(self.resp() == 'HN')
+        for v in one_period: self.port.write(bytearray([v & 255, (v >> 8) & 63]))
+        assert(self.r() == 'HN')
--- fy6800/hello.py
+++ fy6800/hello.py
@@ -1,17 +1,23 @@
 from fy6800 import FY6800, dBm, MHz, kHz, Hz
 
-
 g = FY6800()
+g.off(1)
+g.off(2)
 
-#    _
-#  -/-\_---->
+#
+#  ^   ___
+#  |  /   \
+#  +-/-----\----->
+#  |/       \___
+#
 up = [v << 3 for v in range(2**11)]
 high = [2**14 - 1] * 2**11
 down = [v << 3 for v in range(2**11-1, -1, -1)]
 low = [0] * 2**11
 
-g.arb(62, up+high+down+low)
+g.arb(61, up+high+down+low)
 
-g.req('WMW00')
+g.cmd('WMW00')
 g.a(1, dBm(0))
 g.f(1, MHz(1)+kHz(456))
+g.on(1)
